9684fe4300ba7475f9abe0d32ddd122524838fba
heidiwu3388 <heidiwu3388@yahoo.com>
2022-12-21 15:10:17 -0800

    add route: GET Tag by Id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,9 +18,23 @@ router.get('/', async (req, res) => {
     };
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// GET Tag by Id
+router.get('/:id', async (req, res) => {
+  try{
+    // find a single tag by its `id`
+    const tagData = await Tag.findByPk(req.params.id, {
+      // include its associated Product data
+      include: [{ model: Product }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: `No tag found with id=${req.params.id}` });
+      return;
+    }
+    res.status(200).json(tagData);
+  } 
+  catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 router.post('/', (req, res) => {
