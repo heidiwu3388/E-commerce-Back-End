754bed14e9dbcf0de7fabb61202264f54721fb6c
heidiwu3388 <heidiwu3388@yahoo.com>
2022-12-21 15:25:09 -0800

    add route: GET Product by Id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,10 +18,23 @@ router.get('/', async (req, res) => {
   };
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// GET Product by Id
+router.get('/:id', async (req, res) => {
+  try{
+    // find a single product by its `id`
+    const productData = await Product.findByPk(req.params.id, {
+      // include its associated Category and Tag data
+      include: [{ model: Category }, { model: Tag }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: `No product found with id=${req.params.id}` });
+      return;
+    }
+    res.status(200).json(productData);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
 // create new product
