425d6729d6b33716d02f279d04572de0371ce81c
heidiwu3388 <heidiwu3388@yahoo.com>
2022-12-21 22:25:48 -0800

    add route: DELETE Product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -112,8 +112,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+// DELETE Product
+router.delete('/:id', async (req, res) => {
+  try {
+    // delete one product by its `id` value
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: `No product found with id=${req.params.id}` });
+      return;
+    }
+    res.status(200).json(productData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
